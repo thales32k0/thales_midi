e61c0a4c935129df7c844f826469ae0594a4a880
Thales Eduardo <89024257+thales32k0@users.noreply.github.com>
2023-08-20 10:56:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();  
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-let contador = 0;
-
 
 //para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
@@ -16,7 +22,18 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
 
-    } 
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
